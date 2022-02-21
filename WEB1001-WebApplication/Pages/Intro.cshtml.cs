4a1122fd99a9333d725001d40a78fe7a6a090bc3
oigiselle <gisellesalvadorx@gmail.com>
2022-02-21 00:01:27 -0300

    Add project files.

---
+++ WEB1001-WebApplication/Pages/Intro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEB1001_WebApplication.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
