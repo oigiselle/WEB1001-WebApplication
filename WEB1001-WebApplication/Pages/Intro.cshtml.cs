70c9cc78670e808ae5538597897b0e91a97381db
oigiselle <gisellesalvadorx@gmail.com>
2022-02-21 01:44:41 -0300

    Setting form to ASP

--- WEB1001-WebApplication/Pages/Intro.cshtml.cs
+++ WEB1001-WebApplication/Pages/Intro.cshtml.cs
@@ -3,8 +3,16 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WEB1001_WebApplication.Pages
 {
+
     public class IntroModel : PageModel
     {
+        [FromQuery(Name = "firstValue")]
+        public int firstValue { get; set; }
+
+
+        [FromQuery(Name = "secondValue")]
+        public int secondValue { get; set; }
+
         public void OnGet()
         {
         }
